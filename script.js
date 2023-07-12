465dcbe2c4c4ac6dc2131a3b8e4a9a89ae7c41ed
woodandmortar <moreforblack@gmail.com>
2023-07-12 09:04:03 -0500

    update

--- script.js
+++ script.js
@@ -1,218 +1,245 @@
-$(document).ready(function () {
-  // typing animation
-  (function ($) {
-    $.fn.writeText = function (content) {
-      var contentArray = content.split(""),
-        current = 0,
-        elem = this;
-      setInterval(function () {
-        if (current < contentArray.length) {
-          elem.text(elem.text() + contentArray[current++]);
-        }
-      }, 80);
-    };
-  })(jQuery);
+$( "#nav-btn" ).on( "click", function() {
+  $('#takeover-nav').toggleClass("shown");
+  $('.sticky-nav').toggleClass("difference");
+});
 
-  // input text for typing animation
-  $("#holder").writeText("Political News + Polling");
 
-  // initialize wow.js
-  new WOW().init();
 
-  // Push the body and the nav over by 285px over
-  var main = function () {
-    $(".fa-bars").click(function () {
-      $(".nav-screen").animate(
-        {
-          right: "0px"
-        },
-        200
-      );
+///Initiation Variables
+var icon_1 = document.getElementById("nav-btn");
+var topLine_1 = document.getElementById("top-line-1");
+var middleLine_1 = document.getElementById("middle-line-1");
+var bottomLine_1 = document.getElementById("bottom-line-1");
+var state_1 = "menu";  // can be "menu" or "arrow"
+var topLineY_1;
+var middleLineY_1;
+var bottomLineY_1;
+var topLeftY_1;
+var topRightY_1;
+var bottomLeftY_1;
+var bottomRightY_1;
+var topLeftX_1;
+var topRightX_1;
+var bottomLeftX_1;
+var bottomRightX_1;
 
-      $("body").animate(
-        {
-          right: "285px"
-        },
-        200
-      );
+///Animation Variables
+var segmentDuration_1 = 15;
+var menuDisappearDurationInFrames_1 = segmentDuration_1;
+var arrowAppearDurationInFrames_1 = segmentDuration_1;
+var arrowDisappearDurationInFrames_1 = segmentDuration_1;
+var menuAppearDurationInFrames_1 = segmentDuration_1;
+var menuDisappearComplete_1 = false;
+var arrowAppearComplete_1 = false;
+var arrowDisappearComplete_1 = false;
+var menuAppearComplete_1 = false;
+var currentFrame_1 = 1;
+
+///Menu Disappear 
+function menuDisappearAnimation_1() {
+	currentFrame_1++;
+	if ( currentFrame_1 <= menuDisappearDurationInFrames_1 ) {
+		window.requestAnimationFrame( ()=> { 
+			//top line
+			topLineY_1 = AJS.easeInBack( 37, 50, menuDisappearDurationInFrames_1, currentFrame_1 );
+			topLine_1.setAttribute( "d", "M30,"+topLineY_1+" L70,"+topLineY_1 );
+			//bottom line
+			bottomLineY_1 = AJS.easeInBack( 63, 50, menuDisappearDurationInFrames_1, currentFrame_1 );
+			bottomLine_1.setAttribute( "d", "M30,"+bottomLineY_1+" L70,"+bottomLineY_1 );
+			//recursion
+			menuDisappearAnimation_1();
+		});
+	} else {
+		middleLine_1.style.opacity = "0";
+		currentFrame_1 = 1;
+		menuDisappearComplete_1 = true;
+		openMenuAnimation_1();
+	}
+}
+
+///Cross Appear
+function arrowAppearAnimation_1() {
+	currentFrame_1++;
+	if ( currentFrame_1 <= arrowAppearDurationInFrames_1 ) {
+		window.requestAnimationFrame( ()=> { 
+			//top line
+			topLeftX_1 = AJS.easeOutBack( 30, 35, arrowAppearDurationInFrames_1, currentFrame_1 );
+			topLeftY_1 = AJS.easeOutBack( 50, 35, arrowAppearDurationInFrames_1, currentFrame_1 );
+			bottomRightX_1 = AJS.easeOutBack( 70, 65, arrowAppearDurationInFrames_1, currentFrame_1 );
+			bottomRightY_1 = AJS.easeOutBack( 50, 65, arrowAppearDurationInFrames_1, currentFrame_1 );
+			topLine_1.setAttribute( "d", "M" + topLeftX_1 + "," + topLeftY_1 + " L" + bottomRightX_1 + "," + bottomRightY_1 );
+			//bottom line
+			bottomLeftX_1 = AJS.easeOutBack( 30, 35, arrowAppearDurationInFrames_1, currentFrame_1 );
+			bottomLeftY_1 = AJS.easeOutBack( 50, 65, arrowAppearDurationInFrames_1, currentFrame_1 );
+			topRightX_1 = AJS.easeOutBack( 70, 65, arrowAppearDurationInFrames_1, currentFrame_1 );
+			topRightY_1 = AJS.easeOutBack( 50, 35, arrowAppearDurationInFrames_1, currentFrame_1 );
+			bottomLine_1.setAttribute( "d", "M" + bottomLeftX_1 + "," + bottomLeftY_1 + " L" + topRightX_1 + "," + topRightY_1 );
+			//recursion
+			arrowAppearAnimation_1();
+		});
+	} else {
+		currentFrame_1 = 1;
+		arrowAppearComplete_1 = true;
+		openMenuAnimation_1();
+	}
+}
+
+///Combined Open Menu Animation
+function openMenuAnimation_1() {
+	if ( !menuDisappearComplete_1 ) { 
+		menuDisappearAnimation_1();
+	} else if ( !arrowAppearComplete_1) {
+		arrowAppearAnimation_1();
+	}
+}
+
+///Cross Disappear
+function arrowDisappearAnimation_1() {
+	currentFrame_1++;
+	if ( currentFrame_1 <= arrowDisappearDurationInFrames_1 ) {
+		window.requestAnimationFrame( ()=> {
+			//top line
+			topLeftX_1 = AJS.easeInBack( 35, 30, arrowDisappearDurationInFrames_1, currentFrame_1 );
+			topLeftY_1 = AJS.easeInBack( 35, 50, arrowDisappearDurationInFrames_1, currentFrame_1 );
+			bottomRightX_1 = AJS.easeInBack( 65, 70, arrowDisappearDurationInFrames_1, currentFrame_1 );
+			bottomRightY_1 = AJS.easeInBack( 65, 50, arrowDisappearDurationInFrames_1, currentFrame_1 );
+			topLine_1.setAttribute( "d", "M" + topLeftX_1 + "," + topLeftY_1 + " L" + bottomRightX_1 + "," + bottomRightY_1 );
+			//bottom line
+			bottomLeftX_1 = AJS.easeInBack( 35, 30, arrowDisappearDurationInFrames_1, currentFrame_1 );
+			bottomLeftY_1 = AJS.easeInBack( 65, 50, arrowDisappearDurationInFrames_1, currentFrame_1 );
+			topRightX_1 = AJS.easeInBack( 65, 70, arrowDisappearDurationInFrames_1, currentFrame_1 );
+			topRightY_1 = AJS.easeInBack( 35, 50, arrowDisappearDurationInFrames_1, currentFrame_1 );
+			bottomLine_1.setAttribute( "d", "M" + bottomLeftX_1 + "," + bottomLeftY_1 + " L" + topRightX_1 + "," + topRightY_1 );
+			//recursion
+			arrowDisappearAnimation_1();
+		});
+	} else {
+		middleLine_1.style.opacity = "1";
+		currentFrame_1 = 1;
+		arrowDisappearComplete_1 = true;
+		closeMenuAnimation_1();
+	}
+}
+
+///Menu Appear
+function menuAppearAnimation_1() {
+	currentFrame_1++;
+	if ( currentFrame_1 <= menuAppearDurationInFrames_1 ) {
+		window.requestAnimationFrame( ()=> {
+			//top line
+			topLineY_1 = AJS.easeOutBack( 50, 37, menuDisappearDurationInFrames_1, currentFrame_1 );
+			topLine_1.setAttribute( "d", "M30,"+topLineY_1+" L70,"+topLineY_1 );
+			//bottom line
+			bottomLineY_1 = AJS.easeOutBack( 50, 63, menuDisappearDurationInFrames_1, currentFrame_1 );
+			bottomLine_1.setAttribute( "d", "M30,"+bottomLineY_1+" L70,"+bottomLineY_1 );
+			//recursion
+			menuAppearAnimation_1();
+		});
+	} else {
+		currentFrame_1 = 1;
+		menuAppearComplete_1 = true;
+		closeMenuAnimation_1();
+	}
+}
+
+///Close Menu Animation
+function closeMenuAnimation_1() {
+	if ( !arrowDisappearComplete_1 ) {
+		arrowDisappearAnimation_1();
+	} else if ( !menuAppearComplete_1 ) {
+		menuAppearAnimation_1();
+	}
+}
+
+///Events
+icon_1.addEventListener( "click", ()=> { 
+  if ( state_1 === "menu" ) {
+  	openMenuAnimation_1();
+  	state_1 = "arrow";
+  	arrowDisappearComplete_1 = false;
+		menuAppearComplete_1 = false;
+  } else if ( state_1 === "arrow" ) {
+  	closeMenuAnimation_1();
+  	state_1 = "menu";
+  	menuDisappearComplete_1 = false;
+		arrowAppearComplete_1 = false;
+  }
+});
+
+
+// Cursor
+document.addEventListener("DOMContentLoaded", function(event) {
+  var cursor = document.querySelector(".custom-cursor");
+  var links = document.querySelectorAll("a, button, #nav-btn, input.btn");
+
+  var initCursor = false;
+
+  for (var i = 0; i < links.length; i++) {
+    var selfLink = links[i];
+
+    selfLink.addEventListener("mouseover", function() {
+      cursor.classList.add("custom-cursor--link");
     });
-
-    // Then push them back */
-    $(".fa-times").click(function () {
-      $(".nav-screen").animate(
-        {
-          right: "-285px"
-        },
-        200
-      );
-
-      $("body").animate(
-        {
-          right: "0px"
-        },
-        200
-      );
+    selfLink.addEventListener("mouseout", function() {
+      cursor.classList.remove("custom-cursor--link");
     });
+  }
 
-    $(".nav-links a").click(function () {
-      $(".nav-screen").animate(
-        {
-          right: "-285px"
-        },
-        500
-      );
 
-      $("body").animate(
-        {
-          right: "0px"
-        },
-        500
-      );
+  window.onmousemove = function(e) {
+    var mouseX = e.clientX;
+    var mouseY = e.clientY;
+
+    if (!initCursor) {
+      // cursor.style.opacity = 1;
+      TweenLite.to(cursor, 0.5, {
+        opacity: 1
+      });
+      initCursor = true;
+    }
+
+    TweenLite.to(cursor, 0, {
+      top: mouseY + "px",
+      left: mouseX + "px"
+    });
+  };
+  
+  window.ontouchmove = function(e) {
+    var mouseX = e.touches[0].clientX;
+    var mouseY = e.touches[0].clientY;
+    if (!initCursor) {
+      // cursor.style.opacity = 1;
+      TweenLite.to(cursor, 0.3, {
+        opacity: 1
+      });
+      initCursor = true;
+    }
+
+    TweenLite.to(cursor, 0, {
+      top: mouseY + "px",
+      left: mouseX + "px"
     });
   };
 
-  $(document).ready(main);
-
-  // initiate full page scroll
-
-  $("#fullpage").fullpage({
-    scrollBar: true,
-    responsiveWidth: 400,
-    navigation: true,
-    navigationTooltips: ["home", "about", "polling", "contact", "connect"],
-    anchors: ["home", "about", "polling", "contact", "connect"],
-    menu: "#myMenu",
-    fitToSection: false,
-
-    afterLoad: function (anchorLink, index) {
-      var loadedSection = $(this);
-
-      //using index
-      if (index == 1) {
-        /* add opacity to arrow */
-        $(".fa-chevron-down").each(function () {
-          $(this).css("opacity", "1");
-        });
-        $(".header-links a").each(function () {
-          $(this).css("color", "white");
-        });
-        $(".header-links").css("background-color", "transparent");
-      } else if (index != 1) {
-        $(".header-links a").each(function () {
-          $(this).css("color", "black");
-        });
-        $(".header-links").css("background-color", "white");
-      }
-
-      //using index
-      if (index == 2) {
-        /* animate skill bars */
-        $(".skillbar").each(function () {
-          $(this)
-            .find(".skillbar-bar")
-            .animate(
-              {
-                width: $(this).attr("data-percent")
-              },
-              2500
-            );
-        });
-      }
-    }
-  });
-
-  // move section down one
-  $(document).on("click", "#moveDown", function () {
-    $.fn.fullpage.moveSectionDown();
-  });
-
-  // fullpage.js link navigation
-  $(document).on("click", "#skills", function () {
-    $.fn.fullpage.moveTo(2);
-  });
-
-  $(document).on("click", "#news", function () {
-    $.fn.fullpage.moveTo(3);
-  });
-
-  $(document).on("click", "#contact", function () {
-    $.fn.fullpage.moveTo(4);
-  });
-
-  // smooth scrolling
-  $(function () {
-    $("a[href*=#]:not([href=#])").click(function () {
-      if (
-        location.pathname.replace(/^\//, "") ==
-          this.pathname.replace(/^\//, "") &&
-        location.hostname == this.hostname
-      ) {
-        var target = $(this.hash);
-        target = target.length
-          ? target
-          : $("[name=" + this.hash.slice(1) + "]");
-        if (target.length) {
-          $("html,body").animate(
-            {
-              scrollTop: target.offset().top
-            },
-            700
-          );
-          return false;
-        }
-      }
+  window.onmouseout = function(e) {
+    TweenLite.to(cursor, 0.3, {
+      opacity: 0
     });
-  });
+    initCursor = false;
+  };
 
-  //ajax form
-  $(function () {
-    // Get the form.
-    var form = $("#ajax-contact");
-
-    // Get the messages div.
-    var formMessages = $("#form-messages");
-
-    // Set up an event listener for the contact form.
-    $(form).submit(function (e) {
-      // Stop the browser from submitting the form.
-      e.preventDefault();
-
-      // Serialize the form data.
-      var formData = $(form).serialize();
-
-      // Submit the form using AJAX.
-      $.ajax({
-        type: "POST",
-        url: $(form).attr("action"),
-        data: formData
-      })
-        .done(function (response) {
-          // Make sure that the formMessages div has the 'success' class.
-          $(formMessages).removeClass("error");
-          $(formMessages).addClass("success");
-
-          // Set the message text.
-          $(formMessages).text(response);
-
-          // Clear the form.
-          $("#name").val("");
-          $("#email").val("");
-          $("#message").val("");
-        })
-        .fail(function (data) {
-          // Make sure that the formMessages div has the 'error' class.
-          $(formMessages).removeClass("success");
-          $(formMessages).addClass("error");
-
-          // Set the message text.
-          if (data.responseText !== "") {
-            $(formMessages).text(data.responseText);
-          } else {
-            $(formMessages).text(
-              "Oops! An error occured and your message could not be sent."
-            );
-          }
-        });
+  window.ontouchstart = function(e) {
+    TweenLite.to(cursor, 0.3, {
+      opacity: 1
     });
-  });
+  };
+  
+  window.ontouchend = function(e) {
+    setTimeout( function() {
+      TweenLite.to(cursor, 0.3, {
+        opacity: 0
+      });
+    }, 200);   
+  };
+  
 });
