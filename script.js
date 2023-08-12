18baf2f4b41dd03c29f516d33b7ae6d4e3d99028
woodandmortar <moreforblack@gmail.com>
2023-08-12 09:11:16 -0500

    update

--- script.js
+++ script.js
@@ -1,245 +1,215 @@
-$( "#nav-btn" ).on( "click", function() {
-  $('#takeover-nav').toggleClass("shown");
-  $('.sticky-nav').toggleClass("difference");
-});
+let renderer,
+scene,
+camera,
+sphereBg,
+nucleus,
+stars,
+controls,
+container = document.getElementById("canvas_container"),
+timeout_Debounce,
+noise = new SimplexNoise(),
+cameraSpeed = 0,
+blobScale = 3;
 
 
-
-///Initiation Variables
-var icon_1 = document.getElementById("nav-btn");
-var topLine_1 = document.getElementById("top-line-1");
-var middleLine_1 = document.getElementById("middle-line-1");
-var bottomLine_1 = document.getElementById("bottom-line-1");
-var state_1 = "menu";  // can be "menu" or "arrow"
-var topLineY_1;
-var middleLineY_1;
-var bottomLineY_1;
-var topLeftY_1;
-var topRightY_1;
-var bottomLeftY_1;
-var bottomRightY_1;
-var topLeftX_1;
-var topRightX_1;
-var bottomLeftX_1;
-var bottomRightX_1;
-
-///Animation Variables
-var segmentDuration_1 = 15;
-var menuDisappearDurationInFrames_1 = segmentDuration_1;
-var arrowAppearDurationInFrames_1 = segmentDuration_1;
-var arrowDisappearDurationInFrames_1 = segmentDuration_1;
-var menuAppearDurationInFrames_1 = segmentDuration_1;
-var menuDisappearComplete_1 = false;
-var arrowAppearComplete_1 = false;
-var arrowDisappearComplete_1 = false;
-var menuAppearComplete_1 = false;
-var currentFrame_1 = 1;
-
-///Menu Disappear 
-function menuDisappearAnimation_1() {
-	currentFrame_1++;
-	if ( currentFrame_1 <= menuDisappearDurationInFrames_1 ) {
-		window.requestAnimationFrame( ()=> { 
-			//top line
-			topLineY_1 = AJS.easeInBack( 37, 50, menuDisappearDurationInFrames_1, currentFrame_1 );
-			topLine_1.setAttribute( "d", "M30,"+topLineY_1+" L70,"+topLineY_1 );
-			//bottom line
-			bottomLineY_1 = AJS.easeInBack( 63, 50, menuDisappearDurationInFrames_1, currentFrame_1 );
-			bottomLine_1.setAttribute( "d", "M30,"+bottomLineY_1+" L70,"+bottomLineY_1 );
-			//recursion
-			menuDisappearAnimation_1();
-		});
-	} else {
-		middleLine_1.style.opacity = "0";
-		currentFrame_1 = 1;
-		menuDisappearComplete_1 = true;
-		openMenuAnimation_1();
-	}
-}
-
-///Cross Appear
-function arrowAppearAnimation_1() {
-	currentFrame_1++;
-	if ( currentFrame_1 <= arrowAppearDurationInFrames_1 ) {
-		window.requestAnimationFrame( ()=> { 
-			//top line
-			topLeftX_1 = AJS.easeOutBack( 30, 35, arrowAppearDurationInFrames_1, currentFrame_1 );
-			topLeftY_1 = AJS.easeOutBack( 50, 35, arrowAppearDurationInFrames_1, currentFrame_1 );
-			bottomRightX_1 = AJS.easeOutBack( 70, 65, arrowAppearDurationInFrames_1, currentFrame_1 );
-			bottomRightY_1 = AJS.easeOutBack( 50, 65, arrowAppearDurationInFrames_1, currentFrame_1 );
-			topLine_1.setAttribute( "d", "M" + topLeftX_1 + "," + topLeftY_1 + " L" + bottomRightX_1 + "," + bottomRightY_1 );
-			//bottom line
-			bottomLeftX_1 = AJS.easeOutBack( 30, 35, arrowAppearDurationInFrames_1, currentFrame_1 );
-			bottomLeftY_1 = AJS.easeOutBack( 50, 65, arrowAppearDurationInFrames_1, currentFrame_1 );
-			topRightX_1 = AJS.easeOutBack( 70, 65, arrowAppearDurationInFrames_1, currentFrame_1 );
-			topRightY_1 = AJS.easeOutBack( 50, 35, arrowAppearDurationInFrames_1, currentFrame_1 );
-			bottomLine_1.setAttribute( "d", "M" + bottomLeftX_1 + "," + bottomLeftY_1 + " L" + topRightX_1 + "," + topRightY_1 );
-			//recursion
-			arrowAppearAnimation_1();
-		});
-	} else {
-		currentFrame_1 = 1;
-		arrowAppearComplete_1 = true;
-		openMenuAnimation_1();
-	}
-}
-
-///Combined Open Menu Animation
-function openMenuAnimation_1() {
-	if ( !menuDisappearComplete_1 ) { 
-		menuDisappearAnimation_1();
-	} else if ( !arrowAppearComplete_1) {
-		arrowAppearAnimation_1();
-	}
-}
-
-///Cross Disappear
-function arrowDisappearAnimation_1() {
-	currentFrame_1++;
-	if ( currentFrame_1 <= arrowDisappearDurationInFrames_1 ) {
-		window.requestAnimationFrame( ()=> {
-			//top line
-			topLeftX_1 = AJS.easeInBack( 35, 30, arrowDisappearDurationInFrames_1, currentFrame_1 );
-			topLeftY_1 = AJS.easeInBack( 35, 50, arrowDisappearDurationInFrames_1, currentFrame_1 );
-			bottomRightX_1 = AJS.easeInBack( 65, 70, arrowDisappearDurationInFrames_1, currentFrame_1 );
-			bottomRightY_1 = AJS.easeInBack( 65, 50, arrowDisappearDurationInFrames_1, currentFrame_1 );
-			topLine_1.setAttribute( "d", "M" + topLeftX_1 + "," + topLeftY_1 + " L" + bottomRightX_1 + "," + bottomRightY_1 );
-			//bottom line
-			bottomLeftX_1 = AJS.easeInBack( 35, 30, arrowDisappearDurationInFrames_1, currentFrame_1 );
-			bottomLeftY_1 = AJS.easeInBack( 65, 50, arrowDisappearDurationInFrames_1, currentFrame_1 );
-			topRightX_1 = AJS.easeInBack( 65, 70, arrowDisappearDurationInFrames_1, currentFrame_1 );
-			topRightY_1 = AJS.easeInBack( 35, 50, arrowDisappearDurationInFrames_1, currentFrame_1 );
-			bottomLine_1.setAttribute( "d", "M" + bottomLeftX_1 + "," + bottomLeftY_1 + " L" + topRightX_1 + "," + topRightY_1 );
-			//recursion
-			arrowDisappearAnimation_1();
-		});
-	} else {
-		middleLine_1.style.opacity = "1";
-		currentFrame_1 = 1;
-		arrowDisappearComplete_1 = true;
-		closeMenuAnimation_1();
-	}
-}
-
-///Menu Appear
-function menuAppearAnimation_1() {
-	currentFrame_1++;
-	if ( currentFrame_1 <= menuAppearDurationInFrames_1 ) {
-		window.requestAnimationFrame( ()=> {
-			//top line
-			topLineY_1 = AJS.easeOutBack( 50, 37, menuDisappearDurationInFrames_1, currentFrame_1 );
-			topLine_1.setAttribute( "d", "M30,"+topLineY_1+" L70,"+topLineY_1 );
-			//bottom line
-			bottomLineY_1 = AJS.easeOutBack( 50, 63, menuDisappearDurationInFrames_1, currentFrame_1 );
-			bottomLine_1.setAttribute( "d", "M30,"+bottomLineY_1+" L70,"+bottomLineY_1 );
-			//recursion
-			menuAppearAnimation_1();
-		});
-	} else {
-		currentFrame_1 = 1;
-		menuAppearComplete_1 = true;
-		closeMenuAnimation_1();
-	}
-}
-
-///Close Menu Animation
-function closeMenuAnimation_1() {
-	if ( !arrowDisappearComplete_1 ) {
-		arrowDisappearAnimation_1();
-	} else if ( !menuAppearComplete_1 ) {
-		menuAppearAnimation_1();
-	}
-}
-
-///Events
-icon_1.addEventListener( "click", ()=> { 
-  if ( state_1 === "menu" ) {
-  	openMenuAnimation_1();
-  	state_1 = "arrow";
-  	arrowDisappearComplete_1 = false;
-		menuAppearComplete_1 = false;
-  } else if ( state_1 === "arrow" ) {
-  	closeMenuAnimation_1();
-  	state_1 = "menu";
-  	menuDisappearComplete_1 = false;
-		arrowAppearComplete_1 = false;
-  }
-});
+init();
+animate();
 
 
-// Cursor
-document.addEventListener("DOMContentLoaded", function(event) {
-  var cursor = document.querySelector(".custom-cursor");
-  var links = document.querySelectorAll("a, button, #nav-btn, input.btn");
+function init() {
+    scene = new THREE.Scene();
 
-  var initCursor = false;
+    camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.01, 1000)
+    camera.position.set(0,0,230);
 
-  for (var i = 0; i < links.length; i++) {
-    var selfLink = links[i];
+    const directionalLight = new THREE.DirectionalLight("#fff", 2);
+    directionalLight.position.set(0, 50, -20);
+    scene.add(directionalLight);
 
-    selfLink.addEventListener("mouseover", function() {
-      cursor.classList.add("custom-cursor--link");
+    let ambientLight = new THREE.AmbientLight("#ffffff", 1);
+    ambientLight.position.set(0, 20, 20);
+    scene.add(ambientLight);
+
+    renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha: true
     });
-    selfLink.addEventListener("mouseout", function() {
-      cursor.classList.remove("custom-cursor--link");
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    container.appendChild(renderer.domElement);
+
+    //OrbitControl
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = 4;
+    controls.maxDistance = 350;
+    controls.minDistance = 150;
+    controls.enablePan = false;
+
+    const loader = new THREE.TextureLoader();
+    const textureSphereBg = loader.load('https://i.ibb.co/4gHcRZD/bg3-je3ddz.jpg');
+    const texturenucleus = loader.load('background.png');
+    const textureStar = loader.load("https://i.ibb.co/ZKsdYSz/p1-g3zb2a.png");
+    const texture1 = loader.load("https://i.ibb.co/F8by6wW/p2-b3gnym.png");
+    const texture2 = loader.load("https://i.ibb.co/yYS2yx5/p3-ttfn70.png");
+    const texture4 = loader.load("https://i.ibb.co/yWfKkHh/p4-avirap.png");
+
+
+    /*  Nucleus  */
+    texturenucleus.anisotropy = 16;
+    let icosahedronGeometry = new THREE.IcosahedronGeometry(30, 10);
+    let lambertMaterial = new THREE.MeshPhongMaterial({ map: texturenucleus });
+    nucleus = new THREE.Mesh(icosahedronGeometry, lambertMaterial);
+    scene.add(nucleus);
+
+
+    /*    Sphere  Background   */
+    textureSphereBg.anisotropy = 16;
+    let geometrySphereBg = new THREE.SphereBufferGeometry(150, 40, 40);
+    let materialSphereBg = new THREE.MeshBasicMaterial({
+        side: THREE.BackSide,
+        map: textureSphereBg,
     });
-  }
+    sphereBg = new THREE.Mesh(geometrySphereBg, materialSphereBg);
+    scene.add(sphereBg);
 
 
-  window.onmousemove = function(e) {
-    var mouseX = e.clientX;
-    var mouseY = e.clientY;
+    /*    Moving Stars   */
+    let starsGeometry = new THREE.Geometry();
 
-    if (!initCursor) {
-      // cursor.style.opacity = 1;
-      TweenLite.to(cursor, 0.5, {
-        opacity: 1
-      });
-      initCursor = true;
+    for (let i = 0; i < 50; i++) {
+        let particleStar = randomPointSphere(150);
+
+        particleStar.velocity = THREE.MathUtils.randInt(50, 200);
+
+        particleStar.startX = particleStar.x;
+        particleStar.startY = particleStar.y;
+        particleStar.startZ = particleStar.z;
+
+        starsGeometry.vertices.push(particleStar);
     }
-
-    TweenLite.to(cursor, 0, {
-      top: mouseY + "px",
-      left: mouseX + "px"
+    let starsMaterial = new THREE.PointsMaterial({
+        size: 5,
+        color: "#ffffff",
+        transparent: true,
+        opacity: 0.8,
+        map: textureStar,
+        blending: THREE.AdditiveBlending,
     });
-  };
-  
-  window.ontouchmove = function(e) {
-    var mouseX = e.touches[0].clientX;
-    var mouseY = e.touches[0].clientY;
-    if (!initCursor) {
-      // cursor.style.opacity = 1;
-      TweenLite.to(cursor, 0.3, {
-        opacity: 1
-      });
-      initCursor = true;
+    starsMaterial.depthWrite = false;
+    stars = new THREE.Points(starsGeometry, starsMaterial);
+    scene.add(stars);
+
+
+    /*    Fixed Stars   */
+    function createStars(texture, size, total) {
+        let pointGeometry = new THREE.Geometry();
+        let pointMaterial = new THREE.PointsMaterial({
+            size: size,
+            map: texture,
+            blending: THREE.AdditiveBlending,
+        });
+
+        for (let i = 0; i < total; i++) {
+            let radius = THREE.MathUtils.randInt(149, 70);
+            let particles = randomPointSphere(radius);
+            pointGeometry.vertices.push(particles);
+        }
+        return new THREE.Points(pointGeometry, pointMaterial);
     }
+    scene.add(createStars(texture1, 15, 20));
+    scene.add(createStars(texture2, 5, 5));
+    scene.add(createStars(texture4, 7, 5));
 
-    TweenLite.to(cursor, 0, {
-      top: mouseY + "px",
-      left: mouseX + "px"
-    });
-  };
 
-  window.onmouseout = function(e) {
-    TweenLite.to(cursor, 0.3, {
-      opacity: 0
-    });
-    initCursor = false;
-  };
+    function randomPointSphere (radius) {
+        let theta = 2 * Math.PI * Math.random();
+        let phi = Math.acos(2 * Math.random() - 1);
+        let dx = 0 + (radius * Math.sin(phi) * Math.cos(theta));
+        let dy = 0 + (radius * Math.sin(phi) * Math.sin(theta));
+        let dz = 0 + (radius * Math.cos(phi));
+        return new THREE.Vector3(dx, dy, dz);
+    }
+}
 
-  window.ontouchstart = function(e) {
-    TweenLite.to(cursor, 0.3, {
-      opacity: 1
+
+function animate() {
+
+    //Stars  Animation
+    stars.geometry.vertices.forEach(function (v) {
+        v.x += (0 - v.x) / v.velocity;
+        v.y += (0 - v.y) / v.velocity;
+        v.z += (0 - v.z) / v.velocity;
+
+        v.velocity -= 0.3;
+
+        if (v.x <= 5 && v.x >= -5 && v.z <= 5 && v.z >= -5) {
+            v.x = v.startX;
+            v.y = v.startY;
+            v.z = v.startZ;
+            v.velocity = THREE.MathUtils.randInt(50, 300);
+        }
     });
-  };
-  
-  window.ontouchend = function(e) {
-    setTimeout( function() {
-      TweenLite.to(cursor, 0.3, {
-        opacity: 0
-      });
-    }, 200);   
-  };
-  
+
+
+    //Nucleus Animation
+    nucleus.geometry.vertices.forEach(function (v) {
+        let time = Date.now();
+        v.normalize();
+        let distance = nucleus.geometry.parameters.radius + noise.noise3D(
+            v.x + time * 0.0005,
+            v.y + time * 0.0003,
+            v.z + time * 0.0008
+        ) * blobScale;
+        v.multiplyScalar(distance);
+    })
+    nucleus.geometry.verticesNeedUpdate = true;
+    nucleus.geometry.normalsNeedUpdate = true;
+    nucleus.geometry.computeVertexNormals();
+    nucleus.geometry.computeFaceNormals();
+    nucleus.rotation.y += 0.002;
+
+
+    //Sphere Beckground Animation
+    sphereBg.rotation.x += 0.002;
+    sphereBg.rotation.y += 0.002;
+    sphereBg.rotation.z += 0.002;
+
+
+    controls.update();
+    stars.geometry.verticesNeedUpdate = true;
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+}
+
+
+
+/*     Resize     */
+window.addEventListener("resize", () => {
+    clearTimeout(timeout_Debounce);
+    timeout_Debounce = setTimeout(onWindowResize, 80);
 });
+function onWindowResize() {
+    camera.aspect = container.clientWidth / container.clientHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(container.clientWidth, container.clientHeight);
+}
+
+
+
+/*     Fullscreen btn     */
+// let fullscreen;
+// let fsEnter = document.getElementById('fullscr');
+// fsEnter.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     if (!fullscreen) {
+//         fullscreen = true;
+//         document.documentElement.requestFullscreen();
+//         fsEnter.innerHTML = "Exit Fullscreen";
+//     }
+//     else {
+//         fullscreen = false;
+//         document.exitFullscreen();
+//         fsEnter.innerHTML = "Go Fullscreen";
+//     }
+// });
