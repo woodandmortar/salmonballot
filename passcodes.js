c750c0c4c4c36fc8e745a6f08bb6f0d9cdd8f66f
woodandmortar <moreforblack@gmail.com>
2023-08-24 19:11:34 -0500

    working nations

--- passcodes.js
+++ passcodes.js
@@ -23,14 +23,14 @@ function checkPasscode() {
     document.getElementById("schedule").style.display = "none";
     document.getElementById("secretDiv").style.display = "none";
     scheduleActive();
-    bookFrame2.src = '16122042361017148.html';
+    bookFrame2.src = 'https://mozilla.github.io/pdf.js/web/viewer.html?file=https://woodandmortar.com/salmonballot/nations/NAEAbbzi.pdf';
     alert('Critical notice! Population decrease resolved.  Crisis Level: 0 - Quest 1 Anomaly complete.');
     status = 0;
   }
 
   if(code === "reset9944") {
     scheduleActive();
-    bookFrame2.src = "https://mozilla.github.io/pdf.js/web/viewer.html?file=https://woodandmortar.com/salmonballot/PEAFaxium.pdf";
+    bookFrame2.src = "https://mozilla.github.io/pdf.js/web/viewer.html?file=https://woodandmortar.com/salmonballot/nations/PEAFaxium.pdf";
     alert('Critical notice! Population decrease resolved.  Crisis Level: 0 - Quest 1 Anomaly complete.');
     status = 0;
   }
@@ -44,55 +44,63 @@ function checkPasscode2() {
     document.getElementById("schedule").style.display = "none";
     scheduleActive();
     bookFrame2.src = 'Ventura.html';
+    document.getElementById("venturaID").style.display = "block";
   }
   if(code2 === "Trantum" || code2 === "trantum") {
     document.getElementById("books").style.display = "block";
     document.getElementById("schedule").style.display = "none";
     scheduleActive();
     bookFrame2.src = 'Trantum.html';
+    document.getElementById("trantumID").style.display = "block";
   }
   if(code2 === "Collective" || code2 === "collective") {
     document.getElementById("books").style.display = "block";
     document.getElementById("schedule").style.display = "none";
     scheduleActive();
     bookFrame2.src = 'Collective.html';
+    document.getElementById("Collective.html").style.display = "block";
   }
   if(code2 === "Mercado" || code2 === "mercado") {
     document.getElementById("books").style.display = "block";
     document.getElementById("schedule").style.display = "none";
     scheduleActive();
     bookFrame2.src = 'Mercado.html';
+    document.getElementById("mercadoID").style.display = "block";
   }
   if(code2 === "Faxium" || code2 === "faxium") {
     document.getElementById("books").style.display = "block";
     document.getElementById("schedule").style.display = "none";
     scheduleActive();
-    bookFrame2.src = 'Faxium.html';
+    bookFrame2.src = 'https://mozilla.github.io/pdf.js/web/viewer.html?file=https://woodandmortar.com/salmonballot/nations/PEAFaxium.pdf';
+    document.getElementById("faxiumID").style.display = "block";
   }
   if(code2 === "Iron Coast" || code2 === "iron coast" || code2 === "IronCoast" || code2 === "ironcoast") {
     document.getElementById("books").style.display = "block";
     document.getElementById("schedule").style.display = "none";
     scheduleActive();
     bookFrame2.src = 'IronCoast.html';
+    document.getElementById("ironcoastID").style.display = "block";
   }
   if(code2 === "Eventus" || code2 === "eventus") {
     document.getElementById("books").style.display = "block";
     document.getElementById("schedule").style.display = "none";
     scheduleActive();
     bookFrame2.src = 'Eventus.html';
+    document.getElementById("eventusID").style.display = "block";
   }
   if(code2 === "Abzimuth" || code2 === "abzimuth") {
     document.getElementById("books").style.display = "block";
     document.getElementById("schedule").style.display = "none";
     scheduleActive();
-    bookFrame2.src = 'nations/nibr2531116.html';
+    bookFrame2.src = 'https://mozilla.github.io/pdf.js/web/viewer.html?file=https://woodandmortar.com/salmonballot/nations/NAEAbbzi.pdf';
     document.getElementById("abzimuthID").style.display = "block";
   }
-  if(code2 === "Commune" || code2 === "commune") {
+  if(code2 === "Ocidentica" || code2 === "ocidentica") {
     document.getElementById("books").style.display = "block";
     document.getElementById("schedule").style.display = "none";
     scheduleActive();
-    bookFrame2.src = 'Commune.html';
+    bookFrame2.src = 'ocidentica.html';
+    document.getElementById("ocidenticaID").style.display = "block";
   }
 
 }
@@ -116,13 +124,61 @@ function checkPasscode4() {
 }
 
 
+function getVideos(){
+  document.getElementById("books").style.display = "block";
+  document.getElementById("schedule").style.display = "none";
+  scheduleActive();
+  bookFrame2.src = 'vid004.html';
+}
 
+
+function ventura(){
+  document.getElementById("books").style.display = "block";
+  document.getElementById("schedule").style.display = "none";
+  scheduleActive();
+  bookFrame2.src = 'vid004.html';
+}
+function trantum(){
+  document.getElementById("books").style.display = "block";
+  document.getElementById("schedule").style.display = "none";
+  scheduleActive();
+  bookFrame2.src = 'vid004.html';
+}
+function mercado(){
+  document.getElementById("books").style.display = "block";
+  document.getElementById("schedule").style.display = "none";
+  scheduleActive();
+  bookFrame2.src = 'vid004.html';
+}
+function faxium(){
+  document.getElementById("books").style.display = "block";
+  document.getElementById("schedule").style.display = "none";
+  scheduleActive();
+  bookFrame2.src = 'https://mozilla.github.io/pdf.js/web/viewer.html?file=https://woodandmortar.com/salmonballot/nations/PEAFaxium.pdf';
+}
+function ironcoast(){
+  document.getElementById("books").style.display = "block";
+  document.getElementById("schedule").style.display = "none";
+  scheduleActive();
+  bookFrame2.src = 'vid004.html';
+}
+function eventus(){
+  document.getElementById("books").style.display = "block";
+  document.getElementById("schedule").style.display = "none";
+  scheduleActive();
+  bookFrame2.src = 'vid004.html';
+}
 function abzimuth(){
   document.getElementById("books").style.display = "block";
   document.getElementById("schedule").style.display = "none";
   scheduleActive();
-  bookFrame2.src = 'nations/nibr2531116.html';
-  bookFrame.src = 'nations/nibr2531116.html';
+  bookFrame2.src = 'https://mozilla.github.io/pdf.js/web/viewer.html?file=https://woodandmortar.com/salmonballot/nations/NAEAbbzi.pdf';
+}
+function ocidentica(){
+  document.getElementById("books").style.display = "block";
+  document.getElementById("schedule").style.display = "none";
+  scheduleActive();
+  bookFrame2.src = 'vid004.html';
 }
 
 function collective(){
@@ -130,7 +186,6 @@ function collective(){
   document.getElementById("schedule").style.display = "none";
   scheduleActive();
   bookFrame2.src = 'Collective.html';
-  bookFrame.src = 'Collective.html';
 }
 
 function n002(){
@@ -138,5 +193,4 @@ function n002(){
   document.getElementById("schedule").style.display = "none";
   scheduleActive();
   bookFrame2.src = 'awakening.html';
-  bookFrame.src = 'awakening.html';
 }
